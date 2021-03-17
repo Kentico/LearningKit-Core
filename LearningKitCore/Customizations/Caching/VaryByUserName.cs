e4ca84302d67aca64e40fb8c9b426c380424360e
Jakub Skurek <JakubS@kentico.com>
2021-03-17 10:17:37 +0100

    Kentico Xperience 13 Refresh 1 changes

---
+++ LearningKitCore/Customizations/Caching/VaryByUserName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using CMS.Membership;
+
+using Kentico.PageBuilder.Web.Mvc;
+
+namespace LearningKitCore.Customizations.Caching
+{
+    public class VaryByUserName : ICacheVaryByOption
+    {
+        public string GetKey()
+        {
+            string userFirstName = MembershipContext.AuthenticatedUser?.FirstName ?? String.Empty;
+
+            return $"UserFirstName={userFirstName}";
+        }
+    }
+}
